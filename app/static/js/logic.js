c9452803e2208be206b91094e7c9d60af61a1397
Daniel Herrera <mayan.bridge.consulting@gmail.com>
2021-07-28 15:45:05 +0800

    current for the week of July 30 2021

---
+++ app/static/js/logic.js
@@ -0,0 +1,24 @@
+
+
+//dApp frontend logic
+async function getPrice(){
+    const request_body = {"erc20_address":document.getElementById("tokenAddress").value};
+    const request_moralis = fetch(`${window.origin}/request-price`, {
+        method: "POST",
+        body: JSON.stringify(request_body),
+        headers: new Headers({"content-type": "application/json"})
+        }).then(function (response) {
+            if (response.status !== 200) {
+              console.log(`Looks like there was a problem. Status code: ${response.status}`);
+              return;
+            }
+            response.json().then(function (data) {
+              convert_populate(data);
+            });
+          });   
+}
+
+function convert_populate(_response){
+    result = _response.usdPrice
+    document.getElementById("amountUSD").setAttribute("value", result);
+}
---
+++ app/views.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import render_template, request, make_response
+import os, requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/request-price", methods=["POST"])
+def request_price():
+    req = request.get_json()
+    response = moralis_query(req["erc20_address"],'polygon','mainnet')
+    res = make_response(response, 200)
+    return res
+
+
+
+def moralis_query(_address,_chain,_chain_name):
+    moralis_url=f"https://deep-index.moralis.io/api/token/ERC20/{_address}/price?chain={_chain}&chain_name={_chain_name}"
+    h_values = {'X-API-KEY':os.environ['API_KEY']}
+    r = requests.get(moralis_url, headers=h_values) 
+    return  r.json()
+
